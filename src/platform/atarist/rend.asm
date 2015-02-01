f6a48f66d12353dff4c1a38edf38df8997205790
Mikael Degerfalt <micke@demicode.se>
2015-02-01 16:11:28 +0100

    Placeholder Atari files.

---
+++ src/platform/atarist/rend.asm
@@ -0,0 +1,41 @@
+
+;==============================================================================
+;
+; WaitVsync
+;
+;==============================================================================
+
+rendWaitVSync:
+	rts
+
+
+
+;==============================================================================
+;
+; Set scroll position for both horizontal scroll (X) and vertical scroll (Y)
+;
+; d0=X scroll
+; d1=Y scroll
+;
+;==============================================================================
+
+rendSetScrollXY:
+	rts
+
+
+;==============================================================================
+;
+; Load a sprite from disk to VRAM. This function is responsible for allocating
+; VRAM for the sprite and return some form of handle back to the game so the
+; game have a way to modify sprite properties such as position.
+;
+; Men fanken vet hur. :/
+;
+; d0=File ID
+;
+;==============================================================================
+
+rendLoadSprite:
+	rts
+
+
